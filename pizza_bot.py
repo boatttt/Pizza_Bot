a59db99df49f5f3e027a9c27eb5f2ccc5440143e
boatttt <99222914+boatttt@users.noreply.github.com>
2022-02-10 14:16:21 +1300

    Update pizza_bot.py
    added a random name selector

--- pizza_bot.py
+++ pizza_bot.py
@@ -1,3 +1,12 @@
 # pizza bot program
+import random
+from random import randint
 
+#List of names
 names = ["Naomi", "Mio", "Rosmarie", "Kyle", "Dominicus", "Reigo", "Sheryl", "Lykke", "Hamid", "Sean"]
+
+num = randint(0,9)
+
+name = (names[num])
+
+print(name)
