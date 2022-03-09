a504c43511de2f4064d7e01d6c5aa530b5bc558d
boatttt <99222914+boatttt@users.noreply.github.com>
2022-03-10 12:08:06 +1300

    made delivery function and integrated into the main file
    made delivery v2

--- pizza_bot.py
+++ pizza_bot.py
@@ -54,11 +54,12 @@ def order_type():
             if delivery >= 1 and delivery <= 2:
                 if delivery == 1:
                     print ('Pickup')
-                    pickup()
+                    pickup_info()
                     break
 
                 elif delivery == 2:
                     print ('Delivery')
+                    delivery_info()
                     break
 
             else: 
@@ -71,7 +72,7 @@ def order_type():
 
 
 # Pick up information: name and phone
-def pickup():
+def pickup_info():
     question = ('Please enter your name ')
     customer_details['name'] = not_blank(question )
     #print(customer_details['name'])
@@ -86,8 +87,27 @@ def pickup():
 
 
 # Delivery information: name, address and phone
+def delivery_info():
+    question = ('Please enter your name ')
+    customer_details['name'] = not_blank(question )
+    print(customer_details['name'])
 
+    question = ('Please enter your phone number ')
+    customer_details['phone'] = not_blank(question )
+    print(customer_details['phone'])
 
+    question = ('Please enter your house number ')
+    customer_details['house'] = not_blank(question )
+    print(customer_details['house'])
+
+    question = ('Please enter your street name ')
+    customer_details['street'] = not_blank(question )
+    print(customer_details['street'])
+
+    question = ('Please enter your suburb name ')
+    customer_details['suburb'] = not_blank(question )
+    print(customer_details['suburb'])
+    print(customer_details)
 
 
 
@@ -132,6 +152,6 @@ def main():
     '''
     welcome()
     order_type()
-    pickup()
+    
 
 main()
